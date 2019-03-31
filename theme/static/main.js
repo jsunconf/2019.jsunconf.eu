d368d883df8efb5293a641c52e078db1e36049ea
Emanuel Kluge <mail@emanuel-kluge.de>
2019-03-31 22:22:31 +0200

    fix: finalize conflict resolution
    ...from previous rebase.

--- theme/static/main.js
+++ theme/static/main.js
@@ -8,20 +8,5 @@ if (mapCanvas) {
 }
 
 if (contribootMount) {
-  Promise.all([
-    import('./scripts/contriboot/jsunconf.contriboot.css'),
-    import('./scripts/contriboot/jsunconf.contriboot'),
-  ]).then(
-    () => {},
-    (err) => {
-      // Suppress the error that states, that the CSS could not
-      // be loaded, which is simply not true. o_O
-      if (
-        !err.message.startsWith('Cannot find module') &&
-        !err.message.endsWith(`.css'`)
-      ) {
-        console.error(err);
-      }
-    }
-  );
+  import('./scripts/jsunconf.contriboot').then(() => {});
 }
