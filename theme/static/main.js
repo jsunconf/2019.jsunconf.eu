2aee15fdccfd2b455bdc4ffaf47711cb0a8e5961
Emanuel Kluge <mail@emanuel-kluge.de>
2019-04-02 17:53:27 +0200

    feat: load images on frontpage lazily

--- theme/static/main.js
+++ theme/static/main.js
@@ -2,6 +2,13 @@ import './scripts/slideout-nav';
 
 const mapCanvas = document.getElementById('map-canvas');
 const contribootMount = document.querySelector('.mount');
+const lazyLoadElements = document.querySelectorAll('[data-lazyload-class]');
+
+if (lazyLoadElements.length) {
+  import('./scripts/lazy-load-elements').then(({ lazyLoad }) =>
+    lazyLoad(lazyLoadElements)
+  );
+}
 
 if (mapCanvas) {
   import('./scripts/mapbox').then(({ mapBox }) => mapBox(mapCanvas));
