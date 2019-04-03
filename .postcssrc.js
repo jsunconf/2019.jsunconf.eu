71aa1919bf3fe9f5227672b8883c8b137f79f06f
herschel666 <kraemerseele@gmx.de>
2019-04-03 18:26:41 +0200

    fix: make image URLs absolute
    ...otherwise Safari is not able to load them.

--- .postcssrc.js
+++ .postcssrc.js
@@ -1,3 +1,20 @@
+const postcssUrl = () => {
+  // TODO: share `netlifyUrl`-definition with build.js
+  const netlifyUrlPrefix = process.env.REVIEW_ID
+    ? `deploy-preview-${process.env.REVIEW_ID}--`
+    : '';
+  const netlifyUrl = `https://${netlifyUrlPrefix}amazing-hodgkin-d968dc.netlify.com`;
+  // TODO: do not blindly assume the dev server URL
+  const cdnUrl =
+    process.env.NODE_ENV === 'production'
+      ? netlifyUrl
+      : 'http://localhost:8080';
+
+  return require('postcss-url')({
+    url: ({ url }) => `${cdnUrl}/static/${url}`,
+  });
+};
+
 module.exports = {
-  plugins: [require('autoprefixer')],
+  plugins: [require('autoprefixer'), postcssUrl()],
 };
